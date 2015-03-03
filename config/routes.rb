8d8c98b3c28f91aa694118a6310fb5128c9e7dce
BREESHIA TURNER <BLTURNER@STANFORD.EDU>
2015-03-03 06:09:02 -0800

    add devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+
+  devise_for :users
+  root 'albums#index'
+  get '/about' => 'static_pages#about', as:'about'
   resources :genres
 
   resources :songs
--- db/schema.rb
+++ db/schema.rb
@@ -1,6 +1,3 @@
-
-
-
 # encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
@@ -14,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150212152149) do
+ActiveRecord::Schema.define(version: 20150303140036) do
 
   create_table "albums", force: :cascade do |t|
     t.string   "title"
@@ -22,6 +19,7 @@ ActiveRecord::Schema.define(version: 20150212152149) do
     t.integer  "artist_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "cover"
   end
 
   create_table "artists", force: :cascade do |t|
@@ -46,5 +44,23 @@ ActiveRecord::Schema.define(version: 20150212152149) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "email",                  default: "", null: false
+    t.string   "encrypted_password",     default: "", null: false
+    t.string   "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.integer  "sign_in_count",          default: 0,  null: false
+    t.datetime "current_sign_in_at"
+    t.datetime "last_sign_in_at"
+    t.string   "current_sign_in_ip"
+    t.string   "last_sign_in_ip"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
